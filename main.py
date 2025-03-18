f8737b9e1fdf95d44bdeff5c2da4629e2e991a5c
ZetsumeiIkari <matthew@mackarill.co.uk>
2025-03-18 22:18:57 +0000

    added black window that closes

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
-    
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 0
+    while i == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return    
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+
 if __name__ == "__main__":
     main()
